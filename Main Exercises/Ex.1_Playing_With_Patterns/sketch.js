703f9f4346d9f5d26263fd088504f1a503d51225
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2020-10-17 15:39:48 +0100

    Setting up Canvas for Ex.1

--- Main Exercises/Ex.1_Playing_With_Patterns/sketch.js
+++ Main Exercises/Ex.1_Playing_With_Patterns/sketch.js
@@ -1,7 +1,11 @@
 function setup() {
+    createCanvas(700, 700);
 
+    colorMode(HSB, 360, 100, 100); // Max. HSB
+    smooth(); // Anti-aliasing
 }
 
 function draw() {
+    background(0 ,0, 0);
     
 }
