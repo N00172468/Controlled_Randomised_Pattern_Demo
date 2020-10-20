ddfb203148b780bdfa311198f707e62fe584663f
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2020-10-20 21:04:49 +0100

    Update on Ex.2:
    - Cleaned up codes and added comments

--- Main Exercises/Ex.2_Trigonometry/sketch.js
+++ Main Exercises/Ex.2_Trigonometry/sketch.js
@@ -1,57 +1,47 @@
 function setup() {
     createCanvas(700, 700);
-    // createCanvas(windowWidth, windowHeight);
 
     colorMode(HSB, 360, 100, 100); // Max. HSB
-    // colorMode(HSB, width, height, 100);
     smooth(); // Anti-aliasing
-    rectMode(CENTER);
+    rectMode(CENTER); // Prevents object from rotating on the top-left corner. Enables rotation within centre of object.
 }
 
 function draw() {
-    background(0 ,0, 0);
-    // background(0 ,0, 0, 0.25);
+    background(0 ,0, 0, 0.5);
     stroke(0, 0, 100);
 
     // Setting colours for interpolation:
     let fromColour = color(179, 100, 90); // Start of range
     let toColour = color(299, 100, 100); // End of range
 
+    // Trig. Variables:
     let transValX = width/2;
     let transValY = height/2;
     let rotateVal = atan2(mouseY - transValY, mouseX - transValX);
     
-    // push();
-    //     translate(transValX, transValY);
-    //     // rotate(atan(mouseY / mouseX));
-    //     rotate(rotateVal);
-
-    //     stroke(360, 100, 100);
-    //     strokeWeight(5);
-    //     strokeCap(ROUND);
-    //     fill(0, 0, 10);
-
-    //     rect(0, 0, 100, 100);
-    // pop();
-    
+    // Loop Variables:
     let col = 7;
     let row = 7;
-    let shapeWidth, shapeHeight;
-    shapeWidth = width/col;
-    shapeHeight = height/row;
+    let spaceWidth, spaceHeight;
+    spaceWidth = width/col;
+    spaceHeight = height/row;
 
+    /**
+     * LOOPED SQUARES:
+     * 
+     * - Including:
+     *      - Interpolation
+     *      - Inverse Tan function for rotation
+     */
     for (let y = 0; y < row; y++) {
         for (let x = 0; x < col; x++) {
             push();
-                translate(x * shapeWidth + shapeWidth/2, y * shapeHeight + shapeHeight/2);
+                translate(x * spaceWidth + spaceWidth/2, y * spaceHeight + spaceHeight/2);
                 rotate(rotateVal);
 
-                // stroke(360, 100, 100);
                 stroke(lerpColor(fromColour, toColour, x * y / 32));
                 strokeWeight(5);
-                // strokeCap(ROUND);
-                // noStroke();
-                // fill(0, 0, 10);
+                strokeCap(ROUND);
                 fill(lerpColor(fromColour, toColour, x * y / 32));
                 rect(0, 0, 100, 100);
             pop();
