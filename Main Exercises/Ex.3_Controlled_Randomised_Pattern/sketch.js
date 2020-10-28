a2a950db414365c4d167a2b363ff23a08cef6390
John Carlo Mendoza Ramos <johncarlomramos97@gmail.com>
2020-10-28 18:54:13 +0000

    Update on Ex.3:
    - Created lines pseudo-randomly going to different directions.
    	- Done by rounding the random number of 0 - 1 then creating an
    	if and else statement.
    - randomSeed() is used to return the same psuedo-random numbers every
    time the page resfreshes.

--- Main Exercises/Ex.3_Controlled_Randomised_Pattern/sketch.js
+++ Main Exercises/Ex.3_Controlled_Randomised_Pattern/sketch.js
@@ -13,6 +13,8 @@ function setup() {
 function draw() {
     background(0);
 
+    randomSeed(99); // Setting the seed value to return the same pseudo-random numbers every time.
+    
     /**
      * LOOPED ELLIPSES:
      */
@@ -20,23 +22,24 @@ function draw() {
         for (let i = 0; i < numOfSquares; i++) {
             let transX = i * squareSize + squareSize / 2;
             let transY = j * squareSize + squareSize / 2;
-
+            
             let distance = dist(transX, transY, mouseX, mouseY);
             // let scaleVal = map(distance, 0, cWidth, 15, squareSize * 2);
             let scaleVal = map(distance, 0, cWidth, squareSize, 15);
-
+            
             push();
-                translate(transX, transY);
-                
-                let rotateVal = atan2(mouseY - transY, mouseX - transX);
-                rotate(rotateVal);
-
-                // noStroke();
-                fill(j, i, numOfSquares - j);
-                ellipse(0, 0, scaleVal, scaleVal);
-                // noStroke();
-                stroke(180);
-                ellipse(10, 0, squareSize / 4, squareSize / 4);
+            translate(transX, transY);
+            
+            let randomNum = round(random(0, 1)); // Between Natural numbers of 0 and 1.
+            
+            noFill();
+            stroke(j, i, numOfSquares - j);
+            
+            if (randomNum == 0) {
+                line(0, 0, squareSize, squareSize);
+            } else {
+                line(0, squareSize, squareSize, 0);
+            }
             pop();
         }
     }
